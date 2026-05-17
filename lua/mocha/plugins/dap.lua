809d2e52a15a95a894cf7a908002eef3d6cdc61d
frtzhahn <aldrinalciso2@gmail.com>
2026-05-18 02:35:09 +0800

    feat: re-architect for nvim 0.12 and v8 introspection (fixes #1)

---
+++ lua/mocha/plugins/dap.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+
+    },
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      require('mason-nvim-dap').setup {
+        automatic_installation = true,
+        ensure_installed = { 'js-debug-adapter' },
+      }
+
+      dap.adapters['pwa-node'] = {
+        type = 'server',
+        host = 'localhost',
+        port = '${port}', -- nvim-dap will find an available port
+        executable = {
+          command = vim.fn.stdpath 'data' .. '/mason/bin/js-debug-adapter',
+          args = { '${port}' },
+        },
+      }
+
+			-- keymaps
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+
+			-- dap ui set up
+      dapui.setup()
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+
+			-- language configuration
+			for _, language in ipairs { 'typescript', 'javascript' } do
+        dap.configurations[language] = {
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch file',
+            program = '${file}',
+            cwd = '${workspaceFolder}',
+          },
+        }
+      end
+    end,
+  },
+}
